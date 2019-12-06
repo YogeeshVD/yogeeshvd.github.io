01fcf591967b087c9ff45c84afd8460910ae46a3
YogeeshVD <kekkaryogeesh@gmail.com>
2019-12-06 09:37:12 +0530

    web module

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -1,1 +1,43 @@
 // Include truYum form validation functions here
+
+	function validate() {
+		var nam = document.forms["form1"]["txtName"].value;
+		var prc = document.forms["form1"]["number"].value;
+		var dol= document.forms["form1"]["txtDate"].value;
+		
+		if(nam.trim()=="") {
+			alert(" Name is required.");
+			
+		} else if( prc.trim()=="") {
+			alert(" Price is required.");
+			
+		}else if(dol.trim()=="") {
+			alert(" Date of Launch is required");
+			return false;
+		}else {
+		 true;
+		}
+		
+		if(/[^0-9\-\\/\/]/.test(dol)) {
+			alert("Date is to be a number");
+			return false;
+		} else {
+			true;
+		}
+		
+		if(/[^0-9]/.test(prc)) {
+			alert("Price is to be a number");
+			return false;
+		} else {
+			true;
+		}
+		
+		
+
+		if(nam.length <2 || nam.length >65) {
+			alert("Title should have 2-65 characters");
+			return false; 
+		} else {
+			true;
+		}
+	}
